4c7c29dedb99a9670df869190697b8e3ff047d43
Patrick <patrick.damkat@student.hu.nl>
2025-10-27 17:16:03 +0100

    add function body for selectParser()

--- src/main/kotlin/nl/hu/inno/dashboard/fileparser/application/FileParserService.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/fileparser/application/FileParserService.kt
@@ -1,6 +1,7 @@
 package nl.hu.inno.dashboard.fileparser.application
 
 import nl.hu.inno.dashboard.fileparser.domain.FileParser
+import nl.hu.inno.dashboard.fileparser.domain.exception.FileTypeNotSupportedException
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
@@ -14,6 +15,11 @@ class FileParserService(
     }
 
     private fun selectParser(file: MultipartFile): FileParser {
-        return TODO("Provide the return value")
+        for (parser in fileParsers) {
+            if (parser.supports(file)) {
+                return parser
+            }
+        }
+        throw FileTypeNotSupportedException("No available parser found for file: ${file.originalFilename}")
     }
 }
