3180004d9b8c6cfc13f6e2634c09b18f7a73bed0
Patrick <patrick.damkat@student.hu.nl>
2025-10-27 17:52:01 +0100

    add Apache Commons CSV dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("com.azure.spring:spring-cloud-azure-starter-keyvault")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.apache.commons:commons-csv:1.14.1")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
