259f4041eb7b4504f2bcbf2bc55bdf1ed6e7bcff
Patrick <patrick.damkat@student.hu.nl>
2025-10-27 17:08:01 +0100

    add fileParsers property, add private selectParser() function for parseFile() to use

--- src/main/kotlin/nl/hu/inno/dashboard/fileparser/application/FileParserService.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/fileparser/application/FileParserService.kt
@@ -1,11 +1,19 @@
 package nl.hu.inno.dashboard.fileparser.application
 
+import nl.hu.inno.dashboard.fileparser.domain.FileParser
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
 
 @Service
-class FileParserService() {
+class FileParserService(
+    private val fileParsers: List<FileParser>,
+) {
     fun parseFile(multipartFile: MultipartFile): List<Array<String>> {
+        val parser = selectParser(multipartFile)
+        return parser.parse(multipartFile)
+    }
+
+    private fun selectParser(file: MultipartFile): FileParser {
         return TODO("Provide the return value")
     }
 }
