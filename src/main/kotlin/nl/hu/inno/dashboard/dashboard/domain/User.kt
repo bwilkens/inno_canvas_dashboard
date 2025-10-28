413ae27d031fc782d28e182e30f161045a833d66
Broken-Lad <turan@suslu.eu>
2025-10-28 21:00:26 +0100

    Added: Domein

---
+++ src/main/kotlin/nl/hu/inno/dashboard/dashboard/domain/User.kt
@@ -0,0 +1,37 @@
+package nl.hu.inno.dashboard.dashboard.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.JoinTable
+import jakarta.persistence.Table
+import jakarta.persistence.ManyToMany
+
+@Entity
+@Table(name = "USER")
+data class User (
+    @Id
+    @Column(name = "FIRST_NAME")
+    val firstName: String = "",
+
+    @Column(name = "LAST_NAME")
+    val lastName: String = "",
+
+    @Column(name = "EMAIL_ADDRESS")
+    val emailAdress: String = "",
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "ROLE")
+    val role: Role? = null,
+
+    @ManyToMany
+    @JoinTable(
+        name = "USER_COURSE",
+        joinColumns = [JoinColumn(name = "USER_EMAIL")],
+        inverseJoinColumns = [JoinColumn(name = "COURSE_CANVAS_ID")]
+    )
+    val courses: Set<Course> = emptySet()
+)
