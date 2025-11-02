f9e9dd3a8aad800d12b31c329eb3844bceea72b6
Patrick <patrick.damkat@student.hu.nl>
2025-11-02 15:17:23 +0100

    update api RequestMapping path

--- src/main/kotlin/nl/hu/inno/dashboard/dashboard/presentation/DashboardController.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/dashboard/presentation/DashboardController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
-@RequestMapping("/dashboard/")
+@RequestMapping("/api/v1/dashboard/")
 class DashboardController(
     private val service: DashboardServiceImpl
     ) {
