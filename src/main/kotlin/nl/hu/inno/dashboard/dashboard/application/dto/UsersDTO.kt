6b1a04c4f230ee1e9a3ad51576a9b451c3ae631f
Patrick <patrick.damkat@student.hu.nl>
2025-11-15 16:20:40 +0100

    replace role enum dependency with String value of the role

--- src/main/kotlin/nl/hu/inno/dashboard/dashboard/application/dto/UsersDTO.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/dashboard/application/dto/UsersDTO.kt
@@ -1,12 +1,11 @@
 package nl.hu.inno.dashboard.dashboard.application.dto
 
-import nl.hu.inno.dashboard.dashboard.domain.Role
 import nl.hu.inno.dashboard.dashboard.domain.Users
 
 data class UsersDTO (
     val email: String,
     val name: String,
-    val role: Role?,
+    val role: String,
     val courses: List<CourseDTO> = emptyList()
 ) {
     companion object {
@@ -15,7 +14,7 @@ data class UsersDTO (
             return UsersDTO(
                 email = user.email,
                 name = user.name,
-                role = user.role,
+                role = user.role.name,
                 courses = user.courses.map { CourseDTO.of(it) }
             )
         }
