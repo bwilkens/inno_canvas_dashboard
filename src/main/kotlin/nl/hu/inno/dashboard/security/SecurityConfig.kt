41eb0c91c134f0cdafc5c2d26c9d9321af6415f8
Bilal <b.bakkalikasmi@gmail.com>
2025-11-17 22:37:39 +0100

    addd env variables for now

--- src/main/kotlin/nl/hu/inno/dashboard/security/SecurityConfig.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/security/SecurityConfig.kt
@@ -1,20 +1,25 @@
 package nl.hu.inno.dashboard.security
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.JwtDecoders
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class SecurityConfig {
+class SecurityConfig(
+    @Value("\${azure.tenant-id}")
+    private val tenantId: String
+) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .authorizeHttpRequests { requests ->
                 requests
                     .requestMatchers("/h2-console/**").permitAll()
-                    .anyRequest().permitAll()
-//                    .anyRequest().authenticated()
+                    .anyRequest().authenticated()
             }
             .oauth2Login { }
             .oauth2ResourceServer { resourceServer ->
@@ -23,5 +28,11 @@ class SecurityConfig {
             .csrf { csrf -> csrf.disable() }
             .headers { headers -> headers.frameOptions { it.disable() }}
         return http.build()
+    }
+
+    @Bean
+    fun jwtDecoder(): JwtDecoder {
+        val issuer = "https://login.microsoftonline.com/$tenantId/v2.0"
+        return JwtDecoders.fromIssuerLocation(issuer)
     }
 }
