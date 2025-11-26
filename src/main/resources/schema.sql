d278263eae67ffa8c79991e1b123cd0adc884a38
Patrick <patrick.damkat@student.hu.nl>
2025-11-26 21:36:03 +0100

    delete outdated files

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS COURSE (
-    canvas_id       VARCHAR(10)     PRIMARY KEY,
-    title           VARCHAR,
-    course_code     VARCHAR,
-    start_date      DATE,
-    end_date        DATE
-);
