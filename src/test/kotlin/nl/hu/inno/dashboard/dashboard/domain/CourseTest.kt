cb93d0e1f3e91c560c28fdbd23c988c0f37875eb
Patrick <patrick.damkat@student.hu.nl>
2025-12-04 22:38:35 +0100

    follow given > when > then

--- src/test/kotlin/nl/hu/inno/dashboard/dashboard/domain/CourseTest.kt
+++ src/test/kotlin/nl/hu/inno/dashboard/dashboard/domain/CourseTest.kt
@@ -9,6 +9,7 @@ class CourseTest {
     @Test
     fun factoryMethod_createsCourseCorrectly() {
         val course = Course.of(1, "Test", "Instance", LocalDate.of(2025,1,1), LocalDate.of(2025,12,31))
+
         assertEquals(1, course.canvasCourseId)
         assertEquals("Test", course.courseName)
         assertEquals("Instance", course.instanceName)
@@ -20,6 +21,7 @@ class CourseTest {
     @Test
     fun defaultConstructor_initializesProperties() {
         val course = Course()
+
         assertEquals(0, course.canvasCourseId)
         assertEquals("", course.courseName)
         assertEquals("", course.instanceName)
@@ -33,6 +35,7 @@ class CourseTest {
         val c1 = Course.of(1, "A", "B", LocalDate.MIN, LocalDate.MIN)
         val c2 = Course.of(1, "X", "Y", LocalDate.MAX, LocalDate.MAX)
         val c3 = Course.of(2, "A", "B", LocalDate.MIN, LocalDate.MIN)
+
         assertEquals(c1, c2)
         assertEquals(c1.hashCode(), c2.hashCode())
         assertNotEquals(c1, c3)
@@ -41,12 +44,14 @@ class CourseTest {
     @Test
     fun equals_returnsTrue_whenSameReference() {
         val course = Course.of(1, "Test", "Instance", LocalDate.of(2025,1,1), LocalDate.of(2025,12,31))
+
         assertTrue(course.equals(course))
     }
 
     @Test
     fun equals_returnsFalse_whenComparedToDifferentType() {
         val course = Course.of(1, "Test", "Instance", LocalDate.of(2025,1,1), LocalDate.of(2025,12,31))
+
         assertFalse(course.equals("not a course"))
     }
 }
--- src/test/kotlin/nl/hu/inno/dashboard/dashboard/domain/UsersTest.kt
+++ src/test/kotlin/nl/hu/inno/dashboard/dashboard/domain/UsersTest.kt
@@ -9,6 +9,7 @@ class UsersTest {
     @Test
     fun factoryMethod_createsUserCorrectly() {
         val user = Users.of("Test@hu.nl", "Name", Role.ADMIN)
+
         assertEquals("test@hu.nl", user.email)
         assertEquals("Name", user.name)
         assertEquals(Role.ADMIN, user.role)
@@ -18,6 +19,7 @@ class UsersTest {
     @Test
     fun defaultConstructor_initializesProperties() {
         val user = Users()
+
         assertEquals("", user.email)
         assertEquals("", user.name)
         assertEquals(Role.STUDENT, user.role)
@@ -29,6 +31,7 @@ class UsersTest {
         val u1 = Users.of("a@hu.nl", "A")
         val u2 = Users.of("A@hu.nl", "B")
         val u3 = Users.of("b@hu.nl", "A")
+
         assertEquals(u1, u2)
         assertEquals(u1.hashCode(), u2.hashCode())
         assertNotEquals(u1, u3)
@@ -37,12 +40,14 @@ class UsersTest {
     @Test
     fun equals_returnsTrue_whenSameReference() {
         val user = Users.of("a@hu.nl", "A")
+
         assertTrue(user.equals(user))
     }
 
     @Test
     fun equals_returnsFalse_whenComparedToDifferentType() {
         val user = Users.of("a@hu.nl", "A")
+
         assertFalse(user.equals("a@hu.nl"))
     }
 
@@ -50,7 +55,9 @@ class UsersTest {
     fun linkWithCourse_linksBothSides() {
         val user = Users.of("x@hu.nl", "X")
         val course = Course.of(42, "C", "I", LocalDate.MIN, LocalDate.MIN)
+
         user.linkWithCourse(course)
+
         assertTrue(user.courses.contains(course))
         assertTrue(course.users.contains(user))
     }
@@ -59,8 +66,10 @@ class UsersTest {
     fun linkWithCourse_doesNotDuplicateLinks() {
         val user = Users.of("x@hu.nl", "X")
         val course = Course.of(42, "C", "I", LocalDate.MIN, LocalDate.MIN)
+
         user.linkWithCourse(course)
         user.linkWithCourse(course)
+
         assertEquals(1, user.courses.size)
         assertEquals(1, course.users.size)
     }
