c7a1787c645111561cba563047c831f88f8345d0
Bilal <b.bakkalikasmi@gmail.com>
2026-01-08 17:00:29 +0100

    removed volume

--- prod/db/init.sql
+++
@@ -1,9 +0,0 @@
--- init.sql
-
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE TABLE IF NOT EXISTS users (
-                                     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    email TEXT NOT NULL UNIQUE,
-    created_at TIMESTAMPTZ DEFAULT now()
-    );
