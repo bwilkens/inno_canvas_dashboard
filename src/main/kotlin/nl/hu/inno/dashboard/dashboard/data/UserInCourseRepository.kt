4769fbe6ea7620d16efe386a7d8da8cee9edb20a
Patrick <patrick.damkat@student.hu.nl>
2026-01-11 16:05:25 +0100

    fix type parameter to use the new composite key

--- src/main/kotlin/nl/hu/inno/dashboard/dashboard/data/UserInCourseRepository.kt
+++ src/main/kotlin/nl/hu/inno/dashboard/dashboard/data/UserInCourseRepository.kt
@@ -1,11 +1,12 @@
 package nl.hu.inno.dashboard.dashboard.data
 
 import nl.hu.inno.dashboard.dashboard.domain.UserInCourse
+import nl.hu.inno.dashboard.dashboard.domain.UserInCourseId
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 
-interface UserInCourseRepository : JpaRepository<UserInCourse, Long> {
+interface UserInCourseRepository : JpaRepository<UserInCourse, UserInCourseId> {
     @Modifying
     @Query("DELETE FROM UserInCourse")
     fun deleteAllUserInCourseRecords()
